3c3bd0eb6d7cdd7300df236a1181c8f3afb6f70c
Chuck Hinson <190260+chuckhinson@users.noreply.github.com>
2022-12-19 23:02:05 -0500

    Initial script for installing my base set of tools

---
+++ install-tools.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+set -euo pipefail
+
+function main () {
+
+    # Get OS info (version, code name, etc)
+    source /etc/os-release
+
+    # Make sure we're up to date before we start installing stuff
+    sudo apt update
+    sudo apt upgrade
+
+    installCommonPackages
+
+}
+
+function installCommonPackages() {
+
+  # Install some common packages
+  sudo apt install -y \
+    ca-certificates \
+    curl \
+    net-tools \
+    software-properties-common
+  
+  # (Re)install vim.  The version of vim included with the ubuntu ISO seems not to work 
+  # quite right for me.  Not a vim expert, so this might be the wrong way to solve the
+  # problem, but it works for me.  (Note that this doesnt appear to a problem with 
+  # AWS Ubuntu AMIs, so doing this on an EC2 instance isnt necessary, but doesnt hurt 
+  # anything)
+  sudo apt install -y vim
+
+}
+
+main "$@"
