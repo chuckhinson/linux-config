1e6e2ad8f02217adfab9c03b9a55cf04439cb290
Chuck Hinson <190260+chuckhinson@users.noreply.github.com>
2022-12-23 19:58:27 -0500

    Get rid of updating docker routes fixup - I dont think it's needed anymore

--- openvpn/bin/start-vpn.sh
+++ openvpn/bin/start-vpn.sh
@@ -2,25 +2,44 @@
 
 set -euo pipefail
 
-if (( $( openvpn3 sessions-list | wc -l ) > 1 )) ; then
-  printf "Vpn session already exists\nPlease stop existing session first.\n"
-else
-  openvpn3 session-start -c default
+function setupDockerRoutes {
+  # I'm not sure this is actually needed anymore.  
+  # Once upon a time, I found docker wouldnt run containers when vpn was 
+  # enabled because it would see that vpn had configured the route table 
+  # to route all traffic to the vpn server and docker would assume there 
+  # were no free subnets for it to use for its networking.  That doesnt 
+  # seem to be the case anymore, but I cant remember how to recreate the 
+  # problem (was it just with docker run, or was it only with docker 
+  # compose), so I'm going to leave this code here for now and just not 
+  # invoke it
+
 
   # setup our args for update-docker-route
   # TODO: make these parameters instead of env vars
-  cols=( "$(route | grep ^0.0.0.0 | tr -s " " | cut -d ' ' -f '2 8')" )
+  cols=( $(route | grep ^0.0.0.0 | tr -s " " | cut -d ' ' -f '2 8') )
   route_vpn_gateway=${cols[0]}
   dev=${cols[1]}
   script_type='route-up'
 
   sudo -E ~/.openvpn/bin/update-docker-routes
+}
 
-  # Docker sets up its DNS setting only at startup, so if the hosts DNS
-  # settings change (e.g., when connectint to VPN), you have to restart 
-  # docker to make it use the new settings
-  if systemctl is-active docker ; then
-    sudo systemctl restart docker
+function main {
+
+  if (( $( openvpn3 sessions-list | wc -l ) > 1 )) ; then
+    printf "Vpn session already exists\nPlease stop existing session first.\n"
+  else
+    openvpn3 session-start -c default
+    #setupDockerRoutes
+
+    # Docker sets up its DNS setting only at startup, so if the hosts DNS
+    # settings change (e.g., when connectint to VPN), you have to restart 
+    # docker to make it use the new settings
+    if systemctl is-active docker ; then
+      sudo systemctl restart docker
+    fi
   fi
 
-fi
+}
+
+main "$@"
--- openvpn/bin/stop-vpn.sh
+++ openvpn/bin/stop-vpn.sh
@@ -2,20 +2,41 @@
 
 set -euo pipefail
 
-# setup our args for update-docker-route
-# TODO: make these parameters instead of env vars
-cols=( "$(route | grep ^0.0.0.0 | tr -s " " | cut -d ' ' -f '2 8')" )
-route_vpn_gateway=${cols[0]}
-dev=${cols[1]}
-script_type='route-pre-down'
+function clearDockerRoutes {
+  # I'm not sure this is actually needed anymore.  
+  # Once upon a time, I found docker wouldnt run containers when vpn was 
+  # enabled because it would see that vpn had configured the route table 
+  # to route all traffic to the vpn server and docker would assume there 
+  # were no free subnets for it to use for its networking.  That doesnt 
+  # seem to be the case anymore, but I cant remember how to recreate the 
+  # problem (was it just with docker run, or was it only with docker 
+  # compose), so I'm going to leave this code here for now and just not 
+  # invoke it
 
-sudo -E ~/.openvpn/bin/update-docker-routes
+  # setup our args for update-docker-route
+  # TODO: make these parameters instead of env vars
+  cols=( $(route | grep ^0.0.0.0 | tr -s " " | cut -d ' ' -f '2 8') )
+  route_vpn_gateway=${cols[0]}
+  dev=${cols[1]}
+  script_type='route-pre-down'
 
-openvpn3 session-manage -c default -D
+  sudo -E ~/.openvpn/bin/update-docker-routes
 
-# Docker sets up its DNS setting only at startup, so if the hosts DNS
-# settings change (e.g., when disconnectint from VPN), you have to restart 
-# docker to make it use the new settings
-if systemctl is-active docker ; then
-  sudo systemctl restart docker
-fi
+}
+
+function main {
+
+  #clearDockerRoutes
+
+  openvpn3 session-manage -c default -D
+
+  # Docker sets up its DNS setting only at startup, so if the hosts DNS
+  # settings change (e.g., when disconnectint from VPN), you have to restart 
+  # docker to make it use the new settings
+  if systemctl is-active docker ; then
+    sudo systemctl restart docker
+  fi
+
+}
+
+main "$@"
